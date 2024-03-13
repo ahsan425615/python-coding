92f49c9fc50ab1c1257225d3a56f6267031e9fea
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-03-13 16:12:04 +0500

    data types in python

---
+++ dtypes.py
@@ -0,0 +1,32 @@
+# data types in python
+# numbers : 123445678,  6756.876, 4+8 , decimal(),
+# strinfg : 'spam', "boby", 
+# list: [1,2,3,4,,5]
+# Tuple: (1, 'spam', 4 , 'u')
+# Dictionary: {'food': 'dpam', 'taste', 'yum'}, dict
+# Set : set('abc'),{'a','b','c'}
+# FIle : open('eggs.txt)
+# Boolean:  true , false
+# None : None
+# Functions
+# modules
+# classes
+# Advance: Decoraters, GEnerators, Iterators
+# Metaprogramming
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
